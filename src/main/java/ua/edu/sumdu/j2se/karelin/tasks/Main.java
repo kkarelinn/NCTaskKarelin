725e36cd5d764049e8c9b5e29234a86f88b2d3b0
Andrey Karelin <kkarelinn@gmail.com>
2021-12-09 14:33:58 +0200

    APP-TASK

--- src/main/java/ua/edu/sumdu/j2se/karelin/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/karelin/tasks/Main.java
@@ -14,42 +14,8 @@ public class Main {
 
         MainController mvc = new MainController(new ArrayTaskList(), new MainView());
         mvc.start();
-
-
-
-    }
-    public static void printMap(Map<LocalDateTime, Set<Task>> map){
-        for(Map.Entry<LocalDateTime, Set<Task>> entry : map.entrySet()) {
-            LocalDateTime date = entry.getKey();
-            StringBuilder sb = new StringBuilder(date.toString()).append(": ");
-            Set<Task> tasks = entry.getValue();
-            for (Task t: tasks) {
-                sb.append(t.getTitle()).append(", ");
-            }
-            System.out.println(sb.substring(0, sb.length()-2));
-        }
     }
 
-    public static void writeBinary(String text, File file) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
-            System.out.println("Начинаем работу врайтера....");
-            oos.writeUTF(text);
-            } catch (Exception ex) {
 
-            System.out.println(ex.getMessage());
-        }
-    }
-    public static void readBinary(String text, File file) {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
-            System.out.println("Начинаем работу ридера....");
-            String s = ois.readUTF();
-            System.out.println("-----------");
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-
-        }
-    }
 }
 
